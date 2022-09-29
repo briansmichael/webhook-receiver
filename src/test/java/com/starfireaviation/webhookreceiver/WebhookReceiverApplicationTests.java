e9e89b1c8ce6d502705290bc02d79fd77fb983d1
Brian Michael <brian@cajunbug.net>
2022-09-29 13:43:16 -0400

    Initial checkin

---
+++ src/test/java/com/starfireaviation/webhookreceiver/WebhookReceiverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.starfireaviation.webhookreceiver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebhookReceiverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
